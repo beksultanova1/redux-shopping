6f517d690f07ba0e36ea9263f455ee2ca3205a67
beksultanova1 <eldanabeksultanova18@gmail.com>
2023-05-04 18:19:30 +0600

    cfghvjbk

--- src/components/Product/ProductCard/index.js
+++ src/components/Product/ProductCard/index.js
@@ -21,13 +21,13 @@ const ProductCard = ({product}) => {
         dispatch({type: "ADD_TO_FAVORİTE", payload: product})
     }
     return (
-        <div className="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+        <div className="max-w-sm bg-white border   border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
             <Link to={`/details/${product.id}`}>
-                <img className="rounded-t-lg" src={product.image} alt=""/>
+                <img className="rounded-xl object-cover ml-[40px] my-[20px]"   src={product.image} width={300}   alt=""/>
             </Link>
             <div className="p-5">
                 <a href="#">
-                    <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{product.title}</h5>
+                    <h5 className="mb-2  text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{product.title}</h5>
                 </a>
                 <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">{product.description}</p>
                 <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">{product.price}$</p>
